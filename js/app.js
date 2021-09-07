d3860a1780f58e52a10bc729c43d82e006c0153f
RustamAbykeev <51760919+RustamAbykeev@users.noreply.github.com>
2021-09-07 12:23:03 +0600

    Time&Hours block

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ let MESSAGES = JSON.parse(DATA)
 console.log(MESSAGES)
 const showcaseEl = document.getElementById('showcase')
 const searchFormEl = document.getElementById('searchForm')
+const messagesEl = document.getElementById('messages')
 // const sortSelectEl = document.getElementById('sortSelect')
 renderCards(showcaseEl, MESSAGES)
 
@@ -14,6 +15,42 @@ renderCards(showcaseEl, MESSAGES)
 //     "date": 1609595510000,
 //     "seen": false
 //   },
+function unreadMessagesCounter(status) {
+    const messagesCount = MESSAGES.length
+    let unreadMessages = 0;
+    
+    for (i = 0; i < messagesCount; i++) {
+        if (status === false) {
+            unreadMessages += 1;
+        }
+    }
+    //пробежаться по циклу и прочитать значения свойства seen если false то положить в счетчик +1
+    // если статус true то cчетчик не изменяется
+    return unreadMessages;
+}
+
+
+
+function convertDateToHours(date) {
+    convertedDate = new Date(date);
+    if ((convertedDate.getHours()) < 10) {
+        return '0' + (convertedDate.getHours())
+    }
+        return (convertedDate.getHours())
+} 
+
+function convertDateToMinutes(date) {
+    convertedDate = new Date(date);
+    if ((convertedDate.getMinutes()) < 10) {
+        return '0' + (convertedDate.getMinutes())
+    }
+        return (convertedDate.getMinutes())
+} 
+
+function convertDateFormat(date) {
+    convertedDate = new Date(date);
+    return convertedDate.toLocaleDateString();
+} 
 
 function renderCards(showcaseEl = document, messagesArray = []) {
     showcaseEl.innerHTML = createCardTemplateList(messagesArray).join('')
@@ -32,11 +69,26 @@ function createCardTemplate(messagesData = {}) {
         <a href="tel:${messagesData.phone}" class="phone">${messagesData.phone}</a>
         </div>
         </div>
-        <div class="message-text"><p>${messagesData.text}</p></div>
-        <span>Time</span>
-        <span>Date</span>
+        <div class="message-text">${messagesData.seen ? `<p>${messagesData.text}</p>` : `<p><strong>${messagesData.text}</strong></p>`}</div>
+        <span>${convertDateToHours(messagesData.date)}:${convertDateToMinutes(messagesData.date)}</span>
+        <span>${convertDateFormat(messagesData.date)}</span>
 </div>`
 }
 
 
-//посчитать непрочитанные и прочитанные
+
+searchFormEl.addEventListener('submit', e => {
+    e.preventDefault()
+    const query = e.target.search.value.trim().toLowerCase().split(' ').filter(Boolean)
+    console.log(query);
+    const searchFields = ['name', 'phone', 'text']
+    MESSAGES = JSON.parse(DATA).filter(message => { 
+        return query.every(word => {
+            return searchFields.some(field => {
+                return String(message[field]).toLowerCase().includes(word)
+            })
+        })
+    })
+    // console.table(MESSAGES);
+    renderCards(showcaseEl, MESSAGES);
+})
